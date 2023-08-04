5d8a0bed18cf0bfa243aab433268d342dcc8a59d
WasathTheekshana <wasath.vt@gmail.com>
2023-08-05 01:42:12 +0530

    style: terraform fmt

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "wasath-personal"
     workspaces {
       name = "digitalocean-infra"
