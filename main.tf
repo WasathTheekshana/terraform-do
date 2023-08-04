4e53f500bca4c66b25c7a9f0164d82ac392fc81c
WasathTheekshana <wasath.vt@gmail.com>
2023-08-05 02:10:59 +0530

    fix: geo location in do

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "digitalocean" {
 resource "digitalocean_droplet" "web" {
   image  = "ubuntu-18-04-x64"
   name   = "web-1"
-  region = "nyc2"
+  region = "sfo3"
   size   = "s-1vcpu-1gb"
 }
 
